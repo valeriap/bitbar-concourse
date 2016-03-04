099b13714b6152a77c4bbefec24105d727c1b440
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2016-03-04 22:24:44 +0100

    Send notifications only on state changes
    Use a local YAML::Store to keep the URL and last state of the build

--- lib/concourse/slack/repository.rb
+++ lib/concourse/slack/repository.rb
@@ -1,47 +1,53 @@
 # frozen_string_literal: true
-require 'yaml/store'
 
 module Concourse
   module Slack
     Change = Struct.new(:build, :previous_state) do
       def to_s
-        "Build #{build.name} of #{build.job.name} changed from #{previous_state} to [#{build.status}](#{build.url})"
+        "Build #{build.name} of #{build.job.name} [#{build.status}](#{build.url})"
       end
     end
 
     Addition = Struct.new(:build) do
       def to_s
-        "Build #{build.name} of #{build.job.name} was added as [#{build.status}](#{build.url})"
+        "New build #{build.name} of #{build.job.name} [#{build.status}](#{build.url})"
+      end
+    end
+
+    Removal = Struct.new(:name) do
+      def to_s
+        "Build #{name} was removed"
       end
     end
 
     class Repository
-      def initialize(file_path)
-        @store = YAML::Store.new(file_path)
+      def initialize(store)
+        @store = store
       end
 
-      #
-      # returns builds that have changed, were added or removed since
-      # this method was last called
-      #
       def update(builds)
-        @store.transaction do
-          builds.map do |build|
-            begin
-              previous_state = @store[build.url.to_s]
+        result = builds.map do |build|
+          begin
+            previous_state = @store[build.url.to_s]
 
-              if previous_state.nil?
-                Addition.new(build)
-              else
-                Change.new(build, previous_state) if build.status != previous_state
-              end
-            ensure
-              @store[build.url.to_s] = build.status
+            if previous_state.nil?
+              Addition.new(build)
+            else
+              Change.new(build, previous_state) if build.status != previous_state
             end
-          end.compact
+          ensure
+            @store[build.url.to_s] = build.status
+          end
+        end.compact
 
-          # TODO: FInd removed builds
+        removed = @store.keys - builds.map { |build| build.url.to_s }
+
+        removed.each do |removed_url|
+          result << Removal.new(removed_url)
+          @store.delete(removed_url)
         end
+
+        result
       end
     end
   end
